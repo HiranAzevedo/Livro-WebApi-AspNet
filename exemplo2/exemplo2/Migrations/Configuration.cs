aeec3d2d1d7942f25647a682989439e0ac8d00e3
Hiran Azevedo <hiran.azevedo@vtex.com.br>
2016-06-02 18:39:40 -0300

    Adicionando product e startando EF

---
+++ exemplo2/exemplo2/Migrations/Configuration.cs
@@ -0,0 +1,47 @@
+namespace exemplo2.Migrations
+{
+    using Models;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<exemplo2.Models.exemplo2Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(exemplo2Context context)
+        {
+            context.Products.AddOrUpdate(
+                   p => p.Id,
+                   new Product
+                   {
+                       Id = 1,
+                       Nome = "produto	1",
+                       Codigo = "COD1",
+                       Descricao = "descrição  produto 1",
+                       Preco = 10
+                   },
+
+
+                   new Product
+                   {
+                       Id = 2,
+                       Nome = "produto	2",
+                       Codigo = "COD2",
+                       Descricao = "descrição  produto 2",
+                       Preco = 20
+                   },
+
+
+                   new Product
+                   {
+                       Id = 3,
+                       Nome = "produto	3",
+                       Codigo = "COD3",
+                       Descricao = "descrição  produto 3",
+                       Preco = 30
+                   }                   );
+        }
+    }
+}
