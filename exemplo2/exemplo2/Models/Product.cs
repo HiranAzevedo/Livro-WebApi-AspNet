f8e3b46968a427c1395416a07593eeab438dcca2
Hiran Azevedo <hiran.azevedo@vtex.com.br>
2016-06-02 18:15:28 -0300

    Criando o exemplo 2 com db e EF
    Criando o projeto de webApi

---
+++ exemplo2/exemplo2/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace exemplo2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        [Required]
+        public string Codigo { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
---
+++ exemplo2/exemplo2/Models/exemplo2Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace exemplo2.Models
+{
+    public class exemplo2Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public exemplo2Context() : base("name=exemplo2Context")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
